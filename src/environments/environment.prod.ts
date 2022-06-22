f3b645960d43f41d554829f1d6eaf90af24e07c2
unknown <wesammohamedbehery@gmail.com>
2022-06-22 17:23:59 +0200

    first update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'movie-db-58328',
+    appId: '1:1027675910748:web:fd4c867a6567d3c0c1ec99',
+    storageBucket: 'movie-db-58328.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCsf_57OrqX_KmuWAeVGQgGJWj80bxRSoQ',
+    authDomain: 'movie-db-58328.firebaseapp.com',
+    messagingSenderId: '1027675910748',
+  },
   production: true
 };
